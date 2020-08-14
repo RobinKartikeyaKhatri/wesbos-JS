78d64118702e9a151a489964d317d4acdb00e312
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-08-14 16:12:08 +0530

    Learned about Number data type, and some of opearions and methods available on it

--- playground/types.js
+++ playground/types.js
@@ -1,44 +1,74 @@
-const first = "Robin";
-const middle = 'Kartikeya';
-const last = `khatri`;
+// const first = "Robin";
+// const middle = 'Kartikeya';
+// const last = `khatri`;
 
-// const sentence = 'She said, \'who is there?\'';
-// const sentence2 = "She said, 'who is there?'";
-const sentence3 = `She's so "cool"`;
-console.log(sentence3);
+// // const sentence = 'She said, \'who is there?\'';
+// // const sentence2 = "She said, 'who is there?'";
+// const sentence3 = `She's so "cool"`;
+// console.log(sentence3);
 
-const multiLineStrings = "Twinkle\
-\
-twinkle\
-\
-little\
-\
-star";
+// const multiLineStrings = "Twinkle\
+// \
+// twinkle\
+// \
+// little\
+// \
+// star";
 
-console.log(multiLineStrings);
+// console.log(multiLineStrings);
 
-const multiLineStrings2 = `Twinkle
+// const multiLineStrings2 = `Twinkle
 
-twinkle
+// twinkle
 
-little
-
-
-
-star`;
-
-console.log(multiLineStrings2);
+// little
 
 
-const html = `
-    <div>
-        <h2></h2>
-    </div>
-`;
+
+// star`;
+
+// console.log(multiLineStrings2);
 
 
-const fullName = `Your full name is ${first} ${middle} ${last}`;
-console.log(fullName);
+// const html = `
+//     <div>
+//         <h2></h2>
+//     </div>
+// `;
 
-const greet = "Hello my name is " + first + " " + middle + " " + last;
-console.log(greet);
+
+// const fullName = `Your full name is ${first} ${middle} ${last}`;
+// console.log(fullName);
+
+// const greet = "Hello my name is " + first + " " + middle + " " + last;
+// console.log(greet);
+
+const age = 100;
+const name = "Robin";
+
+console.log(typeof age);
+console.log(typeof name);
+
+
+const a = 10;
+const b = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+console.log(1 +"11");
+
+// Math methods
+
+const number = 125.34;
+
+console.log(number);
+
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.round(number));
+
+console.log(Math.random());
